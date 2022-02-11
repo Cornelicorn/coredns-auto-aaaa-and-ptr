5bd30dc9e7f20de09aadd768b2dcf0e6dc448690
Cornelius Hoffmann <coding@hoffmn.de>
2022-02-11 09:15:45 +0100

    Use caddy v1 provided by coredns

--- autoipv6ptr/setup.go
+++ autoipv6ptr/setup.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
-	"github.com/caddyserver/caddy"
+	"github.com/coredns/caddy"
 )
 
 const AUTOIPV6PTR_PLUGIN_NAME string = "autoipv6ptr"
